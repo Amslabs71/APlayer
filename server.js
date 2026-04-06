6601c62d58d1968693b66d15028842658635f3cd
Amslabs71 <amslabs@proton.me>
2026-04-06 12:51:54 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -339,6 +339,7 @@ function getSecurityHeaders() {
             "media-src 'self' https: http: data: blob:",
             "connect-src 'self' https: http:",
             "frame-src https: http:",
+            "object-src 'none'",
             "worker-src 'self' blob:",
             "form-action 'self'",
             "base-uri 'self'",
